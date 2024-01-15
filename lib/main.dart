1f287035fe68e7ca140b04751270d4aa7380f76b
Dhruv Balchandani <119007749+dhruvx19@users.noreply.github.com>
2024-01-15 23:17:15 +0530

    basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text('Hello World')));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple ,
+        body: Center(
+          child: Text(
+            'Hello World',
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 40),
+          ),
+        ),
+      ),
+    ),
+  );
 }
+
