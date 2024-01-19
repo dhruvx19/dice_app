d5325799e265cd4d951cb881e2f6f0cba862361e
Dhruv Balchandani <119007749+dhruvx19@users.noreply.github.com>
2024-01-19 23:54:25 +0530

    dice_game

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,27 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/text.dart';
 
-
-
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
+
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 41, 1, 110),
-            Color.fromARGB(255, 168, 123, 246)
-          ],
+          colors: [color1, color2],
           begin: Alignment.bottomRight,
           end: Alignment.topLeft,
         ),
       ),
-      child: const Center(
-        child: StyledText()
-      ),
+      child: Center(child: Image.asset('dice-images/dice-2.png')),
     );
   }
 }
 
+
+
+
+
+
+
+
+
+
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: Alignment.bottomRight,
+//           end: Alignment.topLeft,
+//         ),
+//       ),
+//       child: const Center(child: StyledText('hello world')),
+//     );
+//   }
+// }
