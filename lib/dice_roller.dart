900082d916b46c6f9959f43ef22dfa5b7ed62a8b
Dhruv Balchandani <119007749+dhruvx19@users.noreply.github.com>
2024-01-22 23:29:40 +0530

    Update dice_roller.dart

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,42 +1,50 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import
 
-class DiceRoller extends StatefulWidget{
-    // no build method 
-    @override
+class DiceRoller extends StatefulWidget {
+  // no build method
+  const DiceRoller({super.key});
+  @override
   State<DiceRoller> createState() {
-    // TODO: implement createState
-     return
+    return _DiceRollerState();
   }
-} 
+}
 
 //two classes with statefull widgets
 
-class _DiceRollerState extends State<DiceRoller>{
+class _DiceRollerState extends State<DiceRoller> {
+  var activeDiceImage = 'dice-images/dice-2.png';
+
+  void rollDice() {
+    var diceRoll = Random().nextInt(6)+1;
+    setState(() {
+      activeDiceImage = 'dice-images/dice-$diceRoll.png';
+    });
+  }
+
   @override
   Widget build(context) {
     return Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              activeDiceImage,
-              width: 200,
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            TextButton(
-                onPressed: rollDice,
-                style: TextButton.styleFrom(
-                    // padding: const EdgeInsets.only(
-                    //   top: 20,
-                    // ),
-                    foregroundColor: Colors.white,
-                    textStyle: const TextStyle(fontSize: 28)),
-                child: const Text('Roll Dice'))
-          ],
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          activeDiceImage,
+          width: 200,
         ),
-
+        const SizedBox(
+          height: 30,
+        ),
+        TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(
+                //   top: 20,
+                // ),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 28)),
+            child: const Text('Roll Dice'))
+      ],
+    );
   }
 }
-//private class will be used in above class nno matter if you import this file 
+//private class will be used in above class nno matter if you import this file
